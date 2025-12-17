a73b14ea43885f5dc1e920c77477327cd0ecaa78
yama <it242280@trident.ac.jp>
2025-12-17 12:41:15 +0900

    最新

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -3,5 +3,11 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "KitchenLink",
-  description: "キッチンリンクのアプリです",
+   icons: {
+    icon: [
+      { url: "/icon-32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon-48.png", sizes: "48x48", type: "image/png" },
+    ],
+    apple: "/apple-icon.png",
+  },
 };
